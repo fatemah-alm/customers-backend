449cfe3dfed732ad30dd5a199224d369b8a100a6
fatemahAlMomen <fatemah.alm@hotmail.com>
2025-03-18 13:10:20 +0300

    Added error handling middlewares

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ const port = 8000;
 //routes
 app.use("/api/customers", customerRoutes);
 
+//Error handling middleware
+app.use((err, req, res, next) => {
+  res
+    .status(err.status || 500)
+    .json({ message: err.message } || "Internal server error");
+});
+
+//path not found
+app.use((req, res, next) => {
+  res.status(404).json("Path not found");
+});
+
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
